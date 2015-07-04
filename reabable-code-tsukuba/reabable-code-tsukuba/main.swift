3ccb8de33a7c3496cf710e7ee92717822a74c42a
Yu Kadowaki <gates1de@Yus-MacBook-Pro.local>
2015-07-04 15:15:45 +0900

    spec9: feature 'print 4 user recipes'

--- reabable-code-tsukuba/reabable-code-tsukuba/main.swift
+++ reabable-code-tsukuba/reabable-code-tsukuba/main.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+let userCount = 4
+
 func main() {
 
     //TODO: input your file path
@@ -16,23 +18,22 @@ func main() {
     let contentsOfFile = fileOpenWithPath(yourFilePath)
     let recipesArray = splitLine(contentsOfFile)
     let recipesDict = makeRecipesDicWithRecipesArray(recipesArray)
+    
+    var allUserName = Array<String>()
+    var allUserRecipe = Array<Int>()
 
-    let input = getIntByStdIn()
-    if input == 0 {
-        for (var i = 1; i <= count(recipesDict); i++) {
-            println("\(i): " + recipesDict[i]!)
-        }
-    } else {
-        println(recipesDict[input] ?? "Not Found")
+    for doCount in 0..<userCount {
+        let (userName, recipeId) = getStdIn()
+        allUserName.append(userName)
+        allUserRecipe.append(recipeId)
     }
     
-    // これは仕様7のやつ
-//    for (i, recipe) in enumerate(recipesArray) {
-//        println("\(i + 1): \(recipe)")
-//    }
+    printAllUserRecipe(allUserName, allUserRecipe, recipesDict)
+    
 }
 
 func getUserByStdIn(input: NSFileHandle) -> String {
+    let input = NSFileHandle.fileHandleWithStandardInput()
     let userStr = NSString(data: input.availableData, encoding: NSUTF8StringEncoding)
     let userStrScanner = NSScanner(string: userStr! as String)
     let user = userStrScanner.string
@@ -40,18 +41,17 @@ func getUserByStdIn(input: NSFileHandle) -> String {
     return user
 }
 
-func getIntByStdIn() -> Int {
+func getStdIn() -> (String, Int) {
     let input = NSFileHandle.fileHandleWithStandardInput()
     let str = NSString(data: input.availableData, encoding: NSUTF8StringEncoding)
     let scanner = NSScanner(string: str! as String)
     
     // ユーザー名の取得
     let user = getUserByStdIn(input)
-    println("ユーザー名: \(user)")
     
-    var value: Int = 0
-    let result = scanner.scanInteger(&value)
-    return value
+    var recipeId: Int = 0
+    scanner.scanInteger(&recipeId)
+    return (user, recipeId)
 }
 
 func fileOpenWithPath(path: String) -> String {
@@ -70,6 +70,26 @@ func makeRecipesDicWithRecipesArray(recipesArray: [String]) -> [Int: String] {
         recipesDict[id] = recipesArray[i]
     }
     return recipesDict
+}
+
+func printAllUserRecipe(allUserName: Array<String>, allUserRecipe: Array<Int>, recipesDict: Dictionary<Int, String>) {
+    println("---------------------------")
+    
+    for i in 0..<userCount {
+        let userName = allUserName[i]
+        let recipeId = allUserRecipe[i]
+        
+        println("ユーザー名: \(userName)")
+        if recipeId == 0 {
+            for (var i = 1; i <= count(recipesDict); i++) {
+                println("\(i): " + recipesDict[i]!)
+            }
+        } else {
+            println(recipesDict[recipeId] ?? "Not Found")
+        }
+        
+        println("---------------------------")
+    }
 }
 
 main()
