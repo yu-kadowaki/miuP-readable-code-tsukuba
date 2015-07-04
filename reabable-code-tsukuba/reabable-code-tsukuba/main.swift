08ad7e0dcca970fd290d5156ab0098fb2323af95
KazuyaMIURA <miup.0627@gmail.com>
2015-07-04 11:00:56 +0900

    add .gitignore and task 1

---
+++ reabable-code-tsukuba/reabable-code-tsukuba/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  reabable-code-tsukuba
+//
+//  Created by IllyasvielVonEinzbern on 7/4/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import Foundation
+
+println("オムライス")
